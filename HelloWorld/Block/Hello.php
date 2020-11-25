94cf5df9e5f5d22a50fa3565fba8ba95a99774d8
ritu-295 <ritu.mahal@ksolves.com>
2020-11-25 07:54:46 -0500

    Initial Commit

---
+++ HelloWorld/Block/Hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * @package     magento2
+ * @author      Codilar Technologies
+ * @license     https://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ * @link        https://www.codilar.com/
+ */
+
+namespace Knockoutjs\HelloWorld\Block;
+
+
+use Magento\Framework\View\Element\Template;
+
+class Hello extends Template
+{
+
+}
